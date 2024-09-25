ce2d19bd6dd827d974c6fb353ec228b4d78babe3
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2024-09-25 20:37:00 +0000

    adjust plots

--- analysis/plot-quality-time.py
+++ analysis/plot-quality-time.py
@@ -10,12 +10,13 @@ import matplotlib.pyplot as plt
 
 import os
 
-def plot_quality(data, colors, name, figpath):
+def plot_quality(steps, data, colors, name, unit, figpath, numiter):
   plt.figure()
   for inner in data:
-    plt.plot(data[inner], color=colors[inner], label=inner)
-  plt.xlabel("# outer iterations")
+    plt.plot(steps[inner], data[inner], color=colors[inner], label=inner)
+  plt.xlabel(unit)
   plt.ylabel(name)
+  plt.ylim(0, numiter)
   
   plt.legend(loc="best")
   plt.tight_layout()
@@ -36,36 +37,43 @@ inners_colors = {
   8 : "purple"
 }
 
+metrics = ["MS-SSIM", "SSIM", "UQI", "MSE", "PSNR"]
+metric_paths = {
+  "MS-SSIM": 'msssim',
+  "SSIM": 'ssim',
+  "UQI": 'uqi',
+  "MSE": 'mse',
+  "PSNR": 'psnr'
+}
+
 if __name__ == "__main__":
   
-  if len(sys.argv) < 3:
-    print("Usage: python plt-quality-time.py <data folder> <fig folder>")
+  if len(sys.argv) < 4:
+    print("Usage: python plt-quality-time.py <data folder> <fig folder> < num iter>")
     sys.exit(1)
 
 
   datapath = sys.argv[1]
   figpath = sys.argv[2]
+  numiter = sys.argv[3]
 
-  msssims = {}
-  ssims = {}
-  uqis = {}
-  mses = {}
-  psnrs = {}
+  metric_data = {}
+  for metric in metrics:
+    metric_data[metric] = {}
 
   for inner in inner_configures:
     with open(datapath + "/" + inner_data_paths[inner], "rb") as f:
-      msssims[inner] = pickle.load(f)
-      ssims[inner] = pickle.load(f)
-      uqis[inner] = pickle.load(f)
-      mses[inner] = pickle.load(f)
-      psnrs[inner] = pickle.load(f)
+      for metric in metrics:
+        metric_data[metric][inner] = pickle.load(f)
 
-
-  plot_quality(msssims, inners_colors, "MS-SSIM", figpath + "/msssim")
-  plot_quality(ssims, inners_colors, "SSIM", figpath + "/ssim")
-  plot_quality(uqis, inners_colors, "UQI", figpath + "/uqi")
-  plot_quality(mses, inners_colors, "MSE", figpath + "/mse")
-  plot_quality(psnrs, inners_colors, "PSNR", figpath + "/psnr")
+  for metric in metrics:
+    metric_steps = {}
+    metric_steps_adj = {}
+    for inner in inner_configures:
+      metric_steps[inner] = np.array(range(len(metric_data[metric][inner])))
+      metric_steps_adj[inner] = np.array(range(len(metric_data[metric][inner]))) * inner
+    plot_quality(metric_steps, metric_data[metric], inners_colors, metric, "outer iter.", figpath + "/" + metric_paths[metric], numiter)
+    plot_quality(metric_steps_adj, metric_data[metric], inners_colors, metric, "inner iter.", figpath + "/" + "adj-" + metric_paths[metric], numiter*max(inner_configures))
 
 
 
