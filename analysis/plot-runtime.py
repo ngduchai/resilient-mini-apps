a75bd0cf8b5a4056472b98ffbf578618b25b5afa
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2024-10-29 21:47:13 +0000

    add no resilient runtime estimation

--- analysis/plot-runtime.py
+++ analysis/plot-runtime.py
@@ -11,6 +11,32 @@ import matplotlib.pyplot as plt
 
 import os
 
+import scipy.stats as stats
+
+def exp_no_resilient_runtime(lamb, num_processes, runtime):
+  if lamb == 0:
+    return runtime
+  process_success_prob = 1 - stats.expon.cdf(x=runtime, scale=1/lamb)
+  reconstruction_success_prob = stats.binom.pmf(num_processes, num_processes, process_success_prob)
+  exp_runtime = runtime * 1/reconstruction_success_prob
+  fail_runtime = min(runtime, 1/lamb)
+  exp_runtime = runtime + (1 - reconstruction_success_prob)/reconstruction_success_prob*fail_runtime
+  return exp_runtime
+
+def simulate_no_resilient_runtime(lamb, num_processes, runtime):
+  ntries = 10000
+  total_tries = 0
+  for i in range(ntries):
+    count = 0
+    while True:
+      count += 1
+      process_state = np.random.exponential(scale=1/lamb, size=num_processes)
+      process_state = np.sum(np.where(process_state > runtime, 1, 0))
+      if process_state == num_processes:
+        break
+    total_tries += count
+  act_runtime = total_tries / ntries * runtime
+  return act_runtime
 
 # Making a plot showing the checkpoint overhead varying input data size
 def plot_fig(data, xlab, ylab, figpath):
@@ -64,8 +90,51 @@ def plot_fig(data, xlab, ylab, figpath):
   plt.savefig(figpath + ".png")
   plt.savefig(figpath + ".pdf")
 
+def plot_resilient(data, figpath):
+  width = 0.15
+  lapp = None
+  for approach in data:
+    if lapp == None or len(data[approach]["elapsed-time"]) > len(data[lapp]["elapsed-time"]):
+      lapp = approach
+  probs = None
+  plt.figure()
+  m = 0
+  for approach in data:
+    print(approach)
+    appconf = data[approach]
+    appdata = data[approach]["elapsed-time"]
+    total = []
+    prob = []
+    for info in appdata:
+      prob.append(info["prob"])
+      total.append(info["total"])
+    if probs is None:
+      probs = 1/np.array(prob)
+    x = np.arange(len(prob))
+    total = np.array(total)
+    plt.bar(x + width*m, total, width, facecolor="none", edgecolor=appconf["color"], hatch="//", label=appconf["label"])
+    m += 1
+  plt.xlabel("Mean Time to Failure (sec)")
+  plt.xticks(np.arange(len(probs)), probs)
+  plt.ylabel("End-to-end Reconstrucution Time (sec)")
+  plt.yscale("log")
+  
+  plt.legend(loc="best")
+  plt.tight_layout()
+  plt.savefig(figpath + ".png")
+  plt.savefig(figpath + ".pdf")
+
 if __name__ == "__main__":
   
+  # lamb = 0.002
+  # num_processes = 64
+  # runtime = 10
+  # exp_runtime = exp_no_resilient_runtime(lamb, num_processes, runtime)
+  # act_runtime = simulate_no_resilient_runtime(lamb, num_processes, runtime)
+  # print(exp_runtime, act_runtime)
+
+
+
   if len(sys.argv) < 3:
     print("Usage: python plot-time.py <data file> <fig folder>")
     sys.exit(1)
@@ -84,7 +153,24 @@ if __name__ == "__main__":
   plot_fig(plotdata["exp_failure"], "Mean Time to Failure (sec)", "Elapsed time (s)", figpath + "/elapsed-time-no-retry")
   plot_fig(plotdata["with-retries"], "Mean Time to Failure (sec)", "Elapsed time (s)", figpath + "/elapsed-time-with-retry")
   
-  
+  # Calculate runtime for no resilient implementation
+  no_resilience = {}
+  no_resilience["label"] = "No Resilience"
+  no_resilience["color"] = "orange"
+  no_resilience["elapsed-time"] = []
+  ideal_exec_time = 0
+  elapsed_time_info = plotdata["exp_failure"]["veloc"]["elapsed-time"]
+  for info in elapsed_time_info:
+    if info["prob"] == 0:
+      ideal_exec_time = info["total"] - info["ckpt"] - info["comm"]
+  probs = [0, 0.01, 0.05, 0.1]
+  for prob in probs:
+    info = {}
+    info["prob"] = prob
+    info["total"] = exp_no_resilient_runtime(prob, 64, ideal_exec_time)
+    no_resilience["elapsed-time"].append(info)
+  plotdata["exp_failure"]["no-resilient"] = no_resilience
+  plot_resilient(plotdata["exp_failure"], figpath + "/elapsed-time-resilient")
 
 
  
